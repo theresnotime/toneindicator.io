9fe187ba564386baf6d5c3e1cb8bf9eab340ecfa
TheresNoTime <sam@theresnotime.co.uk>
2022-06-23 12:07:58 +0100

    Add gAnalytics

--- templates/main.php
+++ templates/main.php
@@ -20,6 +20,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/style.css">
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VLVMLH4SB8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VLVMLH4SB8');
+    </script>
 </head>
 
 <body class="d-flex h-100 text-center text-white bg-dark vertical-center">
